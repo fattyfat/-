441c1a105fb290f6fac652e4f86e08a2d460c5c6
fattyfat <smalljohn19@hotmail.com>
2017-12-18 00:43:40 +0800

    半
    Signed-off-by: fattyfat <smalljohn19@hotmail.com>

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour {
+
+    // Is this a mine?
+    public bool mine;
+
+    // Different Textures
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    // Use this for initialization
+    void Start () {
+
+        // Randomly decide if it's a mine or not
+        mine = Random.value < 0.15;
+
+        // Register in Grid
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+    }
+
+    // Load another texture
+    public void loadTexture(int adjacentCount)
+    {
+        if (mine)
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        else
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+    }
+
+    // Is it still covered?
+    public bool isCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    void OnMouseUpAsButton()
+    {
+        // It's a mine
+        if (mine)
+        {
+            Grid.uncoverMines();
+            print("you lose");
+        }
+        // It's not a mine
+        else
+        {
+            // ToDo: do stuff..
+        }
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,18 @@
+﻿
+internal class Grid {
+
+    // The Grid itself
+    public static int w = 10; // this is the width
+    public static int h = 13; // this is the height
+    public static Element[,] elements = new Element[w, h];
+
+    // Uncover all Mines
+    public static void uncoverMines()
+    {
+        foreach (Element elem in elements)
+            if (elem.mine)
+                elem.loadTexture(0);
+    }
+}
+
+
